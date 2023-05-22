b9ea233f6430835e3ae278eaf659f3ed0df28661
Xen0mrefne <x3n0g3n1c@gmail.com>
2023-05-22 15:19:38 -0300

    _app comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from "next/app"
 import "../styles/globals.css"
 
 function App({ Component, pageProps}: AppProps) {
-  {/* @ts-expect-error Server Component */}
   return <Component {...pageProps} />
 }
 
